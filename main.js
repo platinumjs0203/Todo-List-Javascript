f5aa89f477af7bb90de07d0711ad6f81cd5a3b37
platinumjs0203 <71059087+platinumjs0203@users.noreply.github.com>
2020-11-29 21:12:58 +0900

    JS: clock

--- main.js
+++ main.js
@@ -1,32 +1,30 @@
+// Greeting
 const insertName = document.querySelector(".insert-name");
 const input = insertName.querySelector(".input");
 const h1 = document.querySelector(".h1");
-const USER_LS = "currentuser"
+const avatar = document.querySelector(".avatar")
 
+const USER_LS = "currentuser"
 function saveName(text){
     localStorage.setItem(USER_LS, text);    
 }
-
 function handleSubmit(a){
-   
     h1.classList.add("none");
     input.classList.add("none");
     h1.classList.add("showing")
     h1.innerHTML = `Hello ${a}`;
+    avatar.classList.add("none");
+    clock.classList.remove("none");
 }
-
 function test(e){
     e.preventDefault();
     const currentValue = input.value;
     handleSubmit(currentValue);
     saveName(currentValue);
 }
-
 function submit(){
-    insertName.addEventListener("submit", test);
-    
+    insertName.addEventListener("submit", test);   
 }
-
 function loadName(){
     const currentuser = localStorage.getItem(USER_LS);
     if(currentuser === null){
@@ -36,7 +34,21 @@ function loadName(){
     }   
 }
 
+//Clock
+const clock = document.querySelector(".clock-js");
+const clockTitle = clock.querySelector("h1");
+
+function getTime(){
+    const d = new Date();
+    const h = d.getHours();
+    const m = d.getMinutes();
+    const s = d.getSeconds();
+    clockTitle.innerText = `${h < 10 ? `0${h}` : h}:${m < 10 ? `0${m}` : m}:${s < 10 ? `0${s}` : s}`
+}
+
 function init(){
-    loadName();    
+    loadName();   
+    getTime(); 
+    setInterval(getTime);
 }
 init();
